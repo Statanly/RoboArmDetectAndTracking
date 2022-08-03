c8ae11451044ac01a8b0624c4023357db2b7c8a1
s70c3 <mari.s70c3@gmail.com>
2022-08-03 13:30:49 +0200

    Bugfix in frame order.

--- yolov5/utils/dataloaders.py
+++ yolov5/utils/dataloaders.py
@@ -193,7 +193,7 @@ class LoadImages:
             img_s0 = cv2.imread(path)
             assert img_f0 is not None, f'Image Not Found {path}'
             s = f'image {self.count}/{self.nf} {path}: '
-        img0 = np.concatenate((img_s0, img_f0), axis=1)
+        img0 = np.concatenate((img_f0, img_s0), axis=1)
         # Padded resize
         img = letterbox(img0, self.img_size, stride=self.stride, auto=self.auto)[0]
 
